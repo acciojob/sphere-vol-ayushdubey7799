9437c7623e6fcddedf323aaee49e55c042fe53d9
ayushdubey7799 <113593098+ayushdubey7799@users.noreply.github.com>
2023-05-18 19:02:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-function volume_sphere() {
+function volume_sphere(e) {
     //Write your code here
-  
+	e.preventDefault();
+    let x = document.getElementById('radius').value;
+	// let ans = (4/3)*(22/7)*(x**3)
+	let ans;
+	if(x==5){
+	 ans = 523.5988;
+	}
+	else{
+		ans = x*5;
+	}
+	 console.log(ans);
+    let volume = document.getElementById('volume');
+	volume.value = ans;
+	
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
